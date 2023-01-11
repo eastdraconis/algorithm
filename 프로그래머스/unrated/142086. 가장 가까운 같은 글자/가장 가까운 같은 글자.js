528cf5cf80eddef72f6dedfd052fbec9bb7e7e7c
DongRyong Han <96985256+Ryong-E@users.noreply.github.com>
2023-01-11 22:59:58 +0900

    [unrated] Title: 가장 가까운 같은 글자, Time: 1.27 ms, Memory: 36.6 MB -BaekjoonHub

--- 프로그래머스/unrated/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.js
+++ 프로그래머스/unrated/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.js
@@ -1,16 +1,10 @@
 function solution(s) {
-    var answer = [];
-    s.split('').forEach((word,index)=>{
-        const result = []
-        
-        if(index > 0){
-            for(let j = index - 1; j >= 0; j--){
-                if(word == s[j]) result.push(index - j)
-            }
-        }
-
-        if(result.length > 0) return answer.push(Math.min(...result))
-        answer.push(-1)
+    const answer = {}
+    
+    return [...s].map((word,i)=>{
+        let result = answer[word] !== undefined ? i - answer[word] : -1
+        answer[word] = i
+        return result
     })
     
     return answer;

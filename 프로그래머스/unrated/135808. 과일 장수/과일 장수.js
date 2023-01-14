6143831ec7596b61d7675d7db901585c9c34cadc
DongRyong Han <96985256+Ryong-E@users.noreply.github.com>
2023-01-14 17:03:05 +0900

    [unrated] Title: 과일 장수, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/unrated/135808. 과일 장수/과일 장수.js
+++ 프로그래머스/unrated/135808. 과일 장수/과일 장수.js
@@ -1,7 +1,5 @@
 function solution(k, m, score) {
-    if(Math.floor(score.length / m) === 0) return 0
-    var answer = 0;
-    const context =Math.floor(score.length / m)
+    let answer = 0;
     const discretion = score.sort((a,b) => b - a)
     
     for(let i = m-1; i < discretion.length; i += m){

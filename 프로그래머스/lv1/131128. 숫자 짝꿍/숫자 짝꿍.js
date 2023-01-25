12a266710bca8aa097528d4f83630154af36595a
DongRyong Han <96985256+Ryong-E@users.noreply.github.com>
2023-01-26 00:42:37 +0900

    [level 1] Title: 숫자 짝꿍, Time: 0.21 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv1/131128. 숫자 짝꿍/숫자 짝꿍.js
+++ 프로그래머스/lv1/131128. 숫자 짝꿍/숫자 짝꿍.js
@@ -1,18 +1,18 @@
 function solution(X, Y) {
-    const a = X.split('')
-    const b = Y.split('')
+    const arrX = X.split('')
+    const arrY = Y.split('')
     const numbers = Array(10).fill().map((v,i) => String(i))
-
     let answer = [];
+    
     numbers.forEach(number =>{
-        const Q = a.filter(elem => elem === number)
-        const W = b.filter(elem => elem === number)
-        if(Q.length !== 0 && W.length !== 0){
-            answer.push(Array(Math.min(Q.length,W.length)).fill(number))
+        const foundNumberX = arrX.filter(elem => elem === number)
+        const foundNumberY = arrY.filter(elem => elem === number)
+        if(foundNumberX.length !== 0 && foundNumberY.length !== 0){
+            answer.push(Array(Math.min(foundNumberX.length,foundNumberY.length)).fill(number))
         }
     })
-    answer = answer.flat().sort((a,b) => b - a)
     
+    answer = answer.flat().sort((a,b) => b - a)
     if(answer.length === 0) return '-1'
     
     return /^0+$/.test(answer.join('')) ? '0' : answer.join('')

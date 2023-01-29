65d1907222d70141ed4db98aeb5b739897445656
DongRyong Han <96985256+Ryong-E@users.noreply.github.com>
2023-01-30 00:18:19 +0900

    [unrated] Title: 기사단원의 무기, Time: 5.37 ms, Memory: 37.3 MB -BaekjoonHub

--- 프로그래머스/unrated/136798. 기사단원의 무기/기사단원의 무기.js
+++ 프로그래머스/unrated/136798. 기사단원의 무기/기사단원의 무기.js
@@ -1,6 +1,6 @@
 function solution(number, limit, power) {
     var answer = 0;
-    for(let i =1; i <= number; i++){
+    for(let i = 1; i <= number; i++){
         const divide = [];
         for(let j = 1; j <= i/2; j++){
             if(i % j === 0){
@@ -9,11 +9,11 @@ function solution(number, limit, power) {
         }
         divide.push(i);
         
-        if(divide.length > limit){
-            answer += power    
-        } else {
-            answer += divide.length
+        if(divide.length > limit) {
+            answer += power;
+            continue;
         }
+        answer += divide.length;
     }
     
     return answer;

683399f702a3f538e00423ff7851a593b7396627
handongryong <eastdraconis@gmail.com>
2023-04-29 16:59:58 +0900

    [level 2] Title: H-Index, Time: 0.05 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/42747. H－Index/H－Index.js
@@ -0,0 +1,17 @@
+function solution(citations) {
+    citations.sort((a,b) => b-a)
+    let answer = 0;
+    for(let i = 0; i < citations.length; i++){
+        if(citations[i] >= i + 1){
+            answer++
+            continue
+        }
+        break;
+    }
+    
+    return answer;
+}
+// if(citations[i] >= i+1 && citations[i] >= (citations.length - i)) {
+//             answer++;
+//             continue;
+//         }

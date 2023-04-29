6aaf58f7503ff477d6a3229618dd3c443b8627e3
handongryong <eastdraconis@gmail.com>
2023-04-29 14:23:25 +0900

    [level 2] Title: 가장 큰 수, Time: 3.05 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv2/42746. 가장 큰 수/가장 큰 수.js
+++ 프로그래머스/lv2/42746. 가장 큰 수/가장 큰 수.js
@@ -3,7 +3,7 @@ function solution(numbers) {
     let answer = [];
     for(let number of numbers)
         answer.push(number.toString());
-    
+    console.log(answer)
     answer.sort(function(a,b){
         return (b+a)-(a+b);
     });
